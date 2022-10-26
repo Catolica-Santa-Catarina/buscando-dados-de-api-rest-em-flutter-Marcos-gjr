2d05abb5839261314be50b3719a1a1d34c5015ad
Marcos Jr <m.goncalves.oficial@gmail.com>
2022-10-26 10:16:18 -0300

    Update: busca de dados atraves de botao

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tempo_template/screens/loading_screen.dart';
+import 'package:geolocator/geolocator.dart';
 
 void main() {
   runApp(const MyApp());
